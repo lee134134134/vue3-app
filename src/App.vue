22fd2db2f8974e152b5d55aba3ccadc29d6b9057
李伟华 <liweihua@qiyee-tech.com>
2022-04-25 11:30:15 +0800

    add: 修改app.vue文件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from "vue";
+const count = ref<number>(0);
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!--  <img alt="Vue logo" src="./assets/logo.png" />-->
+  <p>{{ count }}</p>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
